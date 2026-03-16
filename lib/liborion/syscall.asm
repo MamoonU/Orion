6b012b59f3168a3fdf316793e35bf86abd4c0ac0
moon <mamoonu@hotmail.com>
2026-03-16 20:49:58 +0000

    0.10.2
    Dev Expansion and Liborion
    
    syscall.asm implemented
    start.c implemented

--- lib/liborion/syscall.asm
+++ lib/liborion/syscall.asm
@@ -1,3 +1,22 @@
 ; syscall.asm - liborion syscall mechanism
+[bits 32]
+[section .text]
 
+global syscall
 
+syscall:
+    push    ebx                 ; save registers
+    push    esi
+    push    edi
+
+    mov     eax, [esp+16]       ; syscall number
+    mov     ebx, [esp+20]       ; arg1
+    mov     ecx, [esp+24]       ; arg2
+    mov     edx, [esp+28]       ; arg3
+
+    int     0x80                ; trigger kernel
+
+    pop     edi                 ; restore registers
+    pop     esi
+    pop     ebx
+    ret                         ; return
